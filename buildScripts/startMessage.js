292c1de4d60dbbe5f57fa5521a20fdcd5700c73b
Nareen Babu <mailtobabunareen@gmail.com>
2017-07-16 06:15:48 +0530

    Module 9 in progress

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,7 @@
+// ES 5 - Standard
+//var chalk= require('chalk');
+
+// ES 6 Standard
+import chalk from 'chalk';
+
+console.log(chalk.green("starting app in dev mode")); // eslint-disable-line no-console
---
+++ buildScripts/testSetup.js
@@ -0,0 +1,6 @@
+// For Transpiling
+debugger;
+require('babel-register')();
+
+// For Disaling webpack actions on  CSS
+require.extensions['.css']=function() {};
